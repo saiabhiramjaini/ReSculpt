2e4ce025f4cc9505624fa1dd1eb165da1f7c110c
saiabhiramjaini <abhiramjaini28@gmail.com>
2024-08-04 02:47:43 +0530

    updated

--- server/src/utils/razorpay.ts
+++
@@ -1,7 +0,0 @@
-// import Razorpay from "razorpay";
-
-// export var instance = new Razorpay({
-//     key_id: process.env.RAZORPAY_KEY_ID!,
-//     key_secret: process.env.RAZORPAY_KEY_SECRET!,
-// });
-
